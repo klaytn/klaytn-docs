b32c11faedcfd48c86d42fe0c9ac2384109b2c8e
scott lee <scott.l@klaytn.foundation>
2024-01-03 16:12:57 +0900

    Updated Algolia search API key

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
       algolia: {
         contextualSearch: true,
         appId: '3JXBTKO6ZU',
-        apiKey: '3ae6c772dbecf845225e7ef3f4ac18be',
+        apiKey: 'c5a3c4844a6489a8bf5be1e045adef6d',
         indexName: 'klaytn',
       },
       navbar: {
