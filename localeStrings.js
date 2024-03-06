cb3ad82cd1dbef4749587650a2b1c996424db57e
scott lee <scott.l@klaytn.foundation>
2024-03-06 09:20:04 +0900

    Updated archived docs site's label and link on navbar to appear differently depending on locale

---
+++ localeStrings.js
@@ -0,0 +1,13 @@
+// Links to different archived sites based on locale
+export const navbarItemStrings = {
+    en: "Archived", // Label for English
+    ko: "아카이브 문서", // Label for Korean
+    vi: "Lưu trữ", // Label for Vietnamese
+  };
+  
+  export const legacyDocsLinks = {
+    en: "https://archive-docs.klaytn.foundation/",
+    ko: "https://archive-ko.docs.klaytn.foundation/", // Update with the correct URL
+    vi: "https://archive-vn.docs.klaytn.foundation/", // Update with the correct URL
+  };
+  
