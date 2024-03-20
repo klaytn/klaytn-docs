1c19a36c1e53c3daafc03ce052c5617f1a5b1d28
jack <jack.jin@klaytn.foundation>
2024-03-20 09:14:49 +0900

    Replace original bridge Markdown files with default OpenAPI RPC MDX for local Klaytn-docs setup (#678)
    * Replace original bridge Markdown files with default OpenAPI RPC MDX for local Klaytn-docs setup
    
    * remove dummy files
    
    * control sidebar in local start
    
    * delete dummy openapi spec file

---
+++ docs/references/json-rpc/net/sidebar.ts
@@ -0,0 +1,12 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  apisidebar: [
+    {
+      type: "doc",
+      id: "references/json-rpc/net/web-3-rpc",
+    },
+  ],
+};
+
+export default sidebar.apisidebar;
---
+++ docs/references/json-rpc/personal/sidebar.ts
@@ -0,0 +1,12 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  apisidebar: [
+    {
+      type: "doc",
+      id: "references/json-rpc/personal/web-3-rpc",
+    },
+  ],
+};
+
+export default sidebar.apisidebar;
